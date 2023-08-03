7efc2dcbaf1684c4d00f869eea95b5da3b391aa1
Sunil Jangir <sunil.jangir07@gmail.com>
2023-08-03 11:00:15 +0530

    checked class error resolved

--- script.js
+++ script.js
@@ -3,40 +3,39 @@ const listContainer = document.getElementById("list-container");
 
 function addTask() {
   if (inputBox.value === "") {
-    alert("you must write something!");
+    alert("You must write something!");
   } else {
     let li = document.createElement("li");
     li.innerHTML = inputBox.value;
     listContainer.appendChild(li);
-    //     let span = document.createElement("span");
-    //     span.innerHTML = "\u00d7";
-    //     li.appendChild(span);
-    //   }
-    //   inputBox.value = "";
-    //   saveData();
-    // }
-
-    // listContainer.addEventListener(
-    //   "click",
-    //   function (e) {
-    //     if (e.target.tagName === "LI") {
-    //       e.target.classList.toggle("checked");
-    //       saveData();
-    //     } else if (e.target.tagName === "SPAN") {
-    //       e.target.parentElement.remove();
-    //       saveData();
-    //     }
-    //   },
-    //   false
-    // );
-
-    // function saveData() {
-    //   localStorage.setItem("data", listContainer.innerHTML);
-    // }
-
-    // function showTask() {
-    //   listContainer.innerHTML = localStorage.getItem("data");
-    // }
-    // showTask();
+    let span = document.createElement("span");
+    span.innerHTML = "\u00d7";
+    li.appendChild(span);
   }
+  inputBox.value = "";
+  saveData();
 }
+// Js for the click function
+listContainer.addEventListener(
+  "click",
+  function (e) {
+    if (e.target.tagName === "LI") {
+      e.target.classList.toggle("checked");
+      saveData();
+    } else if (e.target.tagName === "SPAN") {
+      e.target.parentElement.remove();
+      saveData();
+    }
+  },
+  false
+);
+
+// to store the data in browser
+function saveData() {
+  localStorage.setItem("data", listContainer.innerHTML);
+}
+// display the data whenever we open the website or browser
+function showTask() {
+  listContainer.innerHTML = localStorage.getItem("data");
+}
+showTask();
